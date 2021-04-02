10a8eeaeea5233a4da66fdf6f713d9a519bee630
yeongrae <anguk0715@gmail.com>
2021-04-02 14:15:42 +0900

    추가) - 1회 시작 명령에 의해 연속 측정되는 횟수, count - 시작 명령 후 시작 버튼 비활성화 - 설정한 횟수만큼 측정 후 시작 버튼 활성화 - 횟수 변경하여 재시작 가능

--- app/src/main/java/com/example/internetspeedtester/MainActivity.kt
+++ app/src/main/java/com/example/internetspeedtester/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.internetspeedtester
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.internet_speed_testing.InternetSpeedBuilder
 import com.example.internet_speed_testing.ProgressionModel
@@ -10,6 +12,8 @@ import java.lang.Exception
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 /*
 UDP target settings
@@ -17,6 +21,7 @@ UDP target settings
 class Declares {
     val RemoteHost: String = ""
     val RemotePort: Int = 10082
+
     constructor()
     init { }
 }
@@ -24,9 +29,18 @@ val Settings = Declares()
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnStart:Button
+    lateinit var count:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btnStart)
+        count = findViewById(R.id.count)
+        var cnt = 0
+        var limitCount = 0
 
         /*
         *   Internet speed test
@@ -40,11 +54,41 @@ class MainActivity : AppCompatActivity() {
                 if (progressModel.progressTotal.toString() == "100.0") {
 //                    adapter?.setDataList(count, progressModel)
                     Toast.makeText(applicationContext, "$progressModel", Toast.LENGTH_SHORT).show()
-                    getData("${progressModel.downloadSpeed} ${progressModel.uploadSpeed}")
+                    var time = getNowTime()
+                    getData("$time\t${progressModel.downloadSpeed}\t${progressModel.uploadSpeed}")
+                    cnt++
+                    if (cnt == limitCount){
+                        btnStart.isEnabled = true
+                    }
                 }
             }
         })
-        builder.start("ftp://speedtest.tele2.net/1MB.zip", 3)
+
+        btnStart.setOnClickListener {
+            limitCount += count.text.toString().toInt()
+            btnStart.isEnabled = false
+            builder.start("ftp://speedtest.tele2.net/1MB.zip", limitCount)
+        }
+
+
+    }
+
+    /* keep running when the app is paused */
+    override fun onPause() {
+        super.onPause()
+    }
+
+    /* get now time */
+    fun getNowTime(): Any {
+        val current = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            LocalDateTime.now()
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val formatted = current.format(formatter)
+        return  formatted
     }
 
     fun getData(s: String) {
@@ -63,6 +107,8 @@ class MainActivity : AppCompatActivity() {
 //        send(Settings.RemoteHost, Settings.RemotePort, sendData)
     }
 
+
+
     fun send(host: String, port: Int, sendData: ByteArray) {
         var socket: DatagramSocket?=null
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
@@ -80,48 +126,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    inner class UdpDataReceived: Thread() {
-        override fun run() {
-            println("${Thread.currentThread()} Runnable thread started.")
-            while (true) {
-//                receiveUDP()
-                val buffer = ByteArray(2048)
-                var socket: DatagramSocket? = null
-                println("thread")
-                Thread.sleep(100)
-                try {
-                    socket = DatagramSocket(Settings.RemotePort, InetAddress.getByName(Settings.RemoteHost))
-                    socket.broadcast = true
-                    val packet = DatagramPacket(buffer, buffer.size)
-                    socket.receive(packet)
-                    println("Received packet : ${packet.data} // ${packet.address}:${packet.port} // ${packet}")
-                } catch (e: Exception) {
-                    println("Catched exception : ${e.toString()}")
-                    e.printStackTrace()
-                } finally {
-                    socket?.close()
-                }
-            }
-        }
-
-        private fun receiveUDP() {
-            val buffer = ByteArray(2048)
-            var socket: DatagramSocket? = null
-            println("thread")
-            Thread.sleep(1000)
-            try {
-                socket = DatagramSocket(Settings.RemotePort, InetAddress.getByName(Settings.RemoteHost))
-                socket.broadcast = true
-                val packet = DatagramPacket(buffer, buffer.size)
-                socket.receive(packet)
-                println("Received packet : ${packet.data} // ${packet.address}:${packet.port} // ${packet}")
-            } catch (e: Exception) {
-                println("Catched exception : ${e.toString()}")
-                e.printStackTrace()
-            } finally {
-                socket?.close()
-            }
-        }
-    }
+//    inner class UdpDataReceived: Thread() {
+//        override fun run() {
+//            println("${Thread.currentThread()} Runnable thread started.")
+//            while (true) {
+////                receiveUDP()
+//                val buffer = ByteArray(2048)
+//                var socket: DatagramSocket? = null
+//                println("thread")
+//                Thread.sleep(100)
+//                try {
+//                    socket = DatagramSocket(Settings.RemotePort, InetAddress.getByName(Settings.RemoteHost))
+//                    socket.broadcast = true
+//                    val packet = DatagramPacket(buffer, buffer.size)
+//                    socket.receive(packet)
+//                    println("Received packet : ${packet.data} // ${packet.address}:${packet.port} // ${packet}")
+//                } catch (e: Exception) {
+//                    println("Catched exception : ${e.toString()}")
+//                    e.printStackTrace()
+//                } finally {
+//                    socket?.close()
+//                }
+//            }
+//        }
+//
+//        private fun receiveUDP() {
+//            val buffer = ByteArray(2048)
+//            var socket: DatagramSocket? = null
+//            println("thread")
+//            Thread.sleep(1000)
+//            try {
+//                socket = DatagramSocket(Settings.RemotePort, InetAddress.getByName(Settings.RemoteHost))
+//                socket.broadcast = true
+//                val packet = DatagramPacket(buffer, buffer.size)
+//                socket.receive(packet)
+//                println("Received packet : ${packet.data} // ${packet.address}:${packet.port} // ${packet}")
+//            } catch (e: Exception) {
+//                println("Catched exception : ${e.toString()}")
+//                e.printStackTrace()
+//            } finally {
+//                socket?.close()
+//            }
+//        }
+//    }
 
 }
