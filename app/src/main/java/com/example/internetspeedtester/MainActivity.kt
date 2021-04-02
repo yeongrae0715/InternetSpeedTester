0e90b0dfae405eef2d9c95f970a39f777438f39d
yeongrae <anguk0715@gmail.com>
2021-04-02 16:34:40 +0900

    추가) - 대상 기기의 IP 주소 입력 - 대상 기기의 port 입력, 고정 - 송신하는 데이터에 헤더 추가, 시간:t, 다운로드:d, 업로드:u - 버튼 아래에 '송신 완료 횟수 / 설정 횟수' 표시

--- app/src/main/java/com/example/internetspeedtester/MainActivity.kt
+++ app/src/main/java/com/example/internetspeedtester/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.internetspeedtester
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.TextView
 import com.example.internet_speed_testing.InternetSpeedBuilder
 import com.example.internet_speed_testing.ProgressionModel
 import java.lang.Exception
@@ -31,16 +33,26 @@ val Settings = Declares()
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnStart:Button
-    lateinit var count:EditText
+    lateinit var etCount:EditText
+    lateinit var address:EditText
+    lateinit var udpPort:EditText
+    lateinit var curCount:TextView
+//    lateinit var lblProcess:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnStart = findViewById(R.id.btnStart)
-        count = findViewById(R.id.count)
-        var cnt = 0
+        etCount = findViewById(R.id.count)
+        address = findViewById(R.id.address)
+        udpPort = findViewById(R.id.port)
+        curCount = findViewById(R.id.labelCurrent)
+//        lblProcess = findViewById(R.id.labelProcess)
+
+        var totalCnt = 0
         var limitCount = 0
+        var dispCnt = 0
 
         /*
         *   Internet speed test
@@ -49,25 +61,37 @@ class MainActivity : AppCompatActivity() {
         builder.setOnEventInternetSpeedListener(object :
                 InternetSpeedBuilder.OnEventInternetSpeedListener {
             override fun onDownloadProgress(count: Int, progressModel: ProgressionModel) {}
-            override fun onUploadProgress(count: Int, progressModel: ProgressionModel) {}
+            override fun onUploadProgress(count: Int, progressModel: ProgressionModel) {
+//                lblProcess.text = "${progressModel.progressUpload}%"
+            }
             override fun onTotalProgress(count: Int, progressModel: ProgressionModel) {
-                if (progressModel.progressTotal.toString() == "100.0") {
+
+                if (progressModel.progressTotal == 100.0f) {
 //                    adapter?.setDataList(count, progressModel)
-                    Toast.makeText(applicationContext, "$progressModel", Toast.LENGTH_SHORT).show()
+//                    Toast.makeText(applicationContext, "$progressModel", Toast.LENGTH_SHORT).show()
                     var time = getNowTime()
-                    getData("$time\t${progressModel.downloadSpeed}\t${progressModel.uploadSpeed}")
-                    cnt++
-                    if (cnt == limitCount){
+
+                    totalCnt += 1
+
+                    if (totalCnt == limitCount){
                         btnStart.isEnabled = true
                     }
+                    Log.d("Completed", "$totalCnt / $limitCount")
+
+                    dispCnt += 1
+                    curCount.text = "$dispCnt / ${etCount.text}"
+
+                    getData("t$time\td${progressModel.downloadSpeed}\tu${progressModel.uploadSpeed}")
                 }
             }
         })
 
         btnStart.setOnClickListener {
-            limitCount += count.text.toString().toInt()
+            dispCnt = 0
+            limitCount += etCount.text.toString().toInt()
             btnStart.isEnabled = false
             builder.start("ftp://speedtest.tele2.net/1MB.zip", limitCount)
+            curCount.text = "$dispCnt / ${etCount.text}"
         }
 
 
@@ -97,9 +121,11 @@ class MainActivity : AppCompatActivity() {
 //        result.setText(data)
 //        println(data)
 
-        val host = "192.168.0.196"
+//        val host = "192.168.0.196"
 //        val host = "223.171.39.106"
-        val port = 10081
+        val host = address.text.toString()
+//        val port = 10081
+        val port = udpPort.text.toString().toInt()
         val sendData = data.toByteArray()
 //        result.setText(sendData.toString())
 
